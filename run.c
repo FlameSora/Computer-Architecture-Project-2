b01b5e95b3401c3899f371139432c72a870e410d
Min Kim <m-kim@kaist.ac.kr>
2017-10-25 03:27:32 +0900

    fixing 2

--- run.c
+++ run.c
@@ -32,10 +32,48 @@ instruction* get_inst_info(uint32_t pc)
 /*                                                             */
 /***************************************************************/
 void process_instruction(){
-	instruction* min;
-	min  = get_inst_info(CURRENT_STATE.PC);
-	instruction mink = *min;
-	printf("op code is: %d\n",mink.opcode);
+	instruction* instrp;
+	instrp  = get_inst_info(CURRENT_STATE.PC);
+	instruction instr = *instrp;
+	int op = instr.opcode;
+	int func = instr.func_code;
+	int rs = instr.r_t.r_i.rs;
+	int rt = instr.r_t.r_i.rt;
+	int imm = instr.r_t.r_i.r_i.imm;
+	int rd = instr.r_t.r_i.r_i.r.rd;
+	int shamt = instr.r_t.r_i.r_i.r.shamt;
+	int target = instr.r_t.target;	
+	if(instr.opcode == 0){
+		if(func == 8){
+		//jr	
+		}
+		if(func== 33){
+		//Addu
+			CURRENT_STATE.REGS[instr.r_t.r_i.r_i.r.rd];	
+		}
+		if(func== 36){
+		//And
+		}
+		if(func== 39){
+		//Nor
+		}
+		if(func== 37){
+		//Or
+		}
+		if(func== 43){
+		//Sltu
+		}
+		if(func== 0){
+		//Sll
+		}
+		if(func== 2){
+		//Srl
+		}
+		if(func== 35){
+		//Subu
+		}
+	}
+	printf("op code is: %d\n",instr.opcode);
 	printf("pc is: %x\n",CURRENT_STATE.PC);
 ///	printf("hahahahaha");
 	/** Implement this function */
